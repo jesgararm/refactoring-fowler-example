050cc96bd83f68fb74398185f83d778e25deec06
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-05-17 21:45:31 +0200

    Corrección defectos refactor

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -55,4 +55,13 @@ public class Movie {
 		}
 		return thisAmount;
 	}
-}
+
+	int getFrequentRenterPoints(Rental rental) {
+		return getFrequentRenterPoints(rental);
+	}
+
+	int getFrequentRenterPoints(int daysRented) {
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2; 
+		else return 1;
+	}
+	}
--- src/ubu/gii/dass/refactoring/Rental.java
+++ src/ubu/gii/dass/refactoring/Rental.java
@@ -32,8 +32,7 @@ public class Rental {
 	}
 
 	int getFrequentRenterPoints() {
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) return 2; 
-		else return 1;
+		return _movie.getFrequentRenterPoints(getDaysRented());
 	}
 
 }
