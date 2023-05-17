00130b0247114772100f89e3756f25fcd6eef18f
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-05-17 21:07:12 +0200

    Move -> CalculateAmount
    Movemos el método a la clase Movie. Mantenemos referencia en Rental.

--- src/ubu/gii/dass/refactoring/Rental.java
+++ src/ubu/gii/dass/refactoring/Rental.java
@@ -28,23 +28,7 @@ public class Rental {
 	}
 
 	double calculateAmount() {
-		double thisAmount = 0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (getDaysRented() > 2)
-				thisAmount += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (getDaysRented() > 3)
-				thisAmount += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return _movie.calculateAmount(this);
 	}
 
 	int getFrequentRenterPoints() {
